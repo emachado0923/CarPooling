f33cb164228fa50e89471cb280e56069ea44fffb
LeidyAl20 <58597132+LeidyAl20@users.noreply.github.com>
2020-07-08 16:10:33 -0500

    Crear componente Select

--- src/Components/Forms/Select.js
+++ src/Components/Forms/Select.js
@@ -1,70 +1,31 @@
 import React, { Component } from "react";
 import { View, Text, Picker, StyleSheet } from "react-native";
 // Responsive
-import { Grid, Row, Col } from "react-native-easy-grid";
+import { Row } from "react-native-easy-grid";
 
-const Select = ({title})=>{
-  return(
-    <View style={styles.container}>
-        <Text>{title}</Text>
-        <Row style={styles.contSelect}>
-          <Picker style={{
-            width:'100%'
-          }}>
-            <Picker.Item label="Casa" value="Calle 42" />
-            <Picker.Item label="Casa 2" value="Calle 43" />
-          </Picker>
+const Select = ({ borderColor, title, colorText, fontSize }) => {
+  return (
+    <View style={{ marginVertical: 12, }}>
+      <Text style={{
+        color: colorText || "#000",
+        fontWeight: 'bold',
+        fontSize: fontSize || 20
+      }}>{title}</Text>
+      <Row style={{
+        height: 'auto',
+        borderBottomWidth: 2,
+        borderColor: borderColor || '#000',
+      }}>
 
-        </Row>
-      </View>
+        <Picker
+          mode='dropdown'
+          style={{ width: '100%'}}>
+          <Picker.Item label='Algo' value='a' />
+          <Picker.Item label='Prueba' value='a' />
+        </Picker>
+      </Row>
+    </View>
   )
 }
-const styles = StyleSheet.create({
-  container: {
-    width:'100%',
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-  },
-  contSelect: {
-    height:'auto',
-    borderWidth: 1,
-    borderColor: '#00AA37',
-    borderRadius: 5,
-    justifyContent:'center',
-    alignItems:'center',
-    marginVertical: 12,
-  }
-});
 
 export default (Select)
-
-
-// export default class Select extends Component {
-//   constructor() {
-//     super();
-//     this.state = {};
-//   }
-
-//   componentDidMount() { }
-
-//   render() {
-
-//     return (
-//       <View style={styles.container}>
-//         <Text>{}</Text>
-//         <Row style={styles.contSelect}>
-//           <Picker style={{
-//             minWidth: "85%",
-//             maxWidth: "85%",
-//           }}>
-//             <Picker.Item label="Casa" value="Calle 42" />
-//             <Picker.Item label="Casa 2" value="Calle 43" />
-//           </Picker>
-
-//         </Row>
-//       </View>
-//     );
-//   }
-// }
